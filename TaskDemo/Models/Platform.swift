3b1718a064dce971a0d1699660743635d2952eb3
Elsman <ahmedelsman0@gmail.com>
2024-05-14 13:51:38 +0300

    [Feature] create enum for platform so we can handle filtration

---
+++ TaskDemo/Models/Platform.swift
@@ -0,0 +1,12 @@
+//
+//  Platform.swift
+//  TaskDemo
+//
+//  Created by Ahmed Elsman on 14/05/2024.
+//
+
+import Foundation
+
+enum Platform: String, CaseIterable {
+    case all, pc, steam, ios, android
+}
