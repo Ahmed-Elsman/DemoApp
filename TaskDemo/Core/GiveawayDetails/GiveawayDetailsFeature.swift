e2662162f782b74e1f834103be8c146ed02f2b67
Elsman <ahmedelsman0@gmail.com>
2024-05-16 11:14:57 +0300

    [Feature] create init for Giveawaydetails view

--- TaskDemo/Core/GiveawayDetails/GiveawayDetailsFeature.swift
+++ TaskDemo/Core/GiveawayDetails/GiveawayDetailsFeature.swift
@@ -5,4 +5,28 @@
 //  Created by Ahmed Elsman on 15/05/2024.
 //
 
-import Foundation
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct GiveawayDetailsFeature {
+    
+    @ObservableState
+    struct State: Equatable {
+        var giveaway: String?
+    }
+    
+    enum Action {
+        
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+                
+            }
+            return .none
+        }
+    }
+}
--- TaskDemo/Core/GiveawayDetails/GiveawayDetailsView.swift
+++ TaskDemo/Core/GiveawayDetails/GiveawayDetailsView.swift
@@ -6,13 +6,29 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct GiveawayDetailsView: View {
+    
+    let store: StoreOf<GiveawayDetailsFeature>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            NavigationView {
+                Text("\(viewStore.giveaway ?? "")")
+            }
+            .navigationTitle(Text(viewStore.giveaway ?? ""))
+            .onAppear {
+                // add here setting data for view
+            }
+        }
     }
 }
 
 #Preview {
-    GiveawayDetailsView()
+    NavigationStack {
+        GiveawayDetailsView(store: Store(initialState: GiveawayDetailsFeature.State(giveaway: Giveaway.mock.title), reducer: {
+            GiveawayDetailsFeature()
+        }))
+    }
 }
