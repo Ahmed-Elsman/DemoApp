1d493dd133ddc9311769879e78c4e5fbc1bd22b5
Elsman <ahmedelsman0@gmail.com>
2024-05-18 16:08:18 +0300

    [Clean] enhance in details of giveaway ui to semi fit but it need more enhancments

--- TaskDemo/Core/GiveawayDetails/GiveawayDetailsFeature.swift
+++ TaskDemo/Core/GiveawayDetails/GiveawayDetailsFeature.swift
@@ -13,18 +13,38 @@ import SwiftUI
 struct GiveawayDetailsFeature {
     
     struct State: Equatable {
-        var giveaway: Giveaway?
+        var giveaway: Giveaway
+        var imageLoaderState = ImageLoaderFeature.State()
     }
     
     enum Action {
         case setGiveawayDetails(Giveaway)
+        case imageLoaderAction(ImageLoaderFeature.Action)
+        case setGiveawayImage(String)
+        case setGiveawayImageContentMode(ContentMode)
     }
     
     var body: some ReducerOf<Self> {
+        
+        Scope(state: \.imageLoaderState, action: \.imageLoaderAction) {
+            ImageLoaderFeature()
+        }
+        
+        
         Reduce { state, action in
             switch action {
             case let .setGiveawayDetails(giveaway):
                 state.giveaway = giveaway
+                return .none
+            case let .setGiveawayImage(image):
+                return .run { send in
+                    await send(.imageLoaderAction(.setImageUrlString(image)))
+                }
+            case let .setGiveawayImageContentMode(contentMode):
+                return .run { send in
+                    await send(.imageLoaderAction(.setcontentMode(contentMode)))
+                }
+            case .imageLoaderAction:
                 return .none
             }
         }
--- TaskDemo/Core/GiveawayDetails/GiveawayDetailsView.swift
+++ TaskDemo/Core/GiveawayDetails/GiveawayDetailsView.swift
@@ -11,15 +11,105 @@ import ComposableArchitecture
 struct GiveawayDetailsView: View {
     
     let store: StoreOf<GiveawayDetailsFeature>
-    
+
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
             NavigationView {
-                Text("\(viewStore.giveaway?.title ?? "")")
+                ZStack(alignment: .top) {
+                    VStack(alignment: .leading) {
+                        ZStack(alignment: .top) {
+                            
+                            ImageLoaderView(store: store.scope(state: \.imageLoaderState, action: \.imageLoaderAction))
+//                            .frame(width: viewStore.imageSize, height: viewStore.isCarousel ? viewStore.imageSize/2 : viewStore.imageSize)
+                                .overlay(
+                                    LinearGradient(
+                                        gradient: Gradient(colors: [Color.black.opacity(0.5), Color.black.opacity(0.5), Color.black.opacity(0.5)]),
+                                        startPoint: .top,
+                                        endPoint: .bottom
+                                    )
+                                )
+                            
+                            HStack {
+                                Spacer()
+                                Button(action: {
+                                    // Action for bookmark button
+                                }) {
+                                    Image(systemName: "heart")
+                                        .padding()
+                                        .background(Color.black.opacity(0.5))
+                                        .clipShape(Circle())
+                                }
+                                .padding(.top, 10)
+                                .padding(.trailing, 10)
+                            }
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                            
+                            HStack {
+                                VStack {
+                                    Image(systemName: "dollarsign.circle.fill")
+                                    Text("\(viewStore.giveaway.worth)")
+                                }
+                                
+                                Spacer()
+                                
+                                VStack {
+                                    Image(systemName: "person.2.fill")
+                                    Text("\(viewStore.giveaway.users)")
+                                }
+                                
+                                Spacer()
+                                
+                                VStack {
+                                    Image(systemName: "gamecontroller.fill")
+                                    Text("\(viewStore.giveaway.type)")
+                                }
+                            }
+                            .font(.title2)
+                            .foregroundColor(.black)
+                            
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text("Platforms")
+                                    .font(.subheadline)
+                                    .fontWeight(.bold)
+                                
+                                Text("\(viewStore.giveaway.platforms)")
+                                    .font(.subheadline)
+                                
+                                Text("Giveaway End Date")
+                                    .font(.subheadline)
+                                    .fontWeight(.bold)
+                                
+                                Text("\(viewStore.giveaway.endDate)")
+                                    .font(.subheadline)
+                                
+                                Text("Description")
+                                    .font(.subheadline)
+                                    .fontWeight(.bold)
+                                
+                                Text(viewStore.giveaway.description)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            .padding(.top, 10)
+                        }
+                        .padding()
+                    }
+                    .background(Color.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 5)
+                    .padding()
+                }
+                .background(Color.white.ignoresSafeArea())
             }
-            .navigationTitle(Text(viewStore.giveaway?.title ?? ""))
             .onAppear {
-                // add here setting data for view
+                viewStore.send(.setGiveawayImage(viewStore.giveaway.image))
+                viewStore.send(.setGiveawayImageContentMode(ContentMode.fill))
             }
         }
     }
